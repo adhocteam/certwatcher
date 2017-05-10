f7c339883b18a8eba003c9aa11d41a37bfa5b702
Paul Smith <paulsmith@pobox.com>
2017-05-10 12:20:01 -0500

    Simplify test cert gen

--- main_test.go
+++ main_test.go
@@ -43,18 +43,13 @@ func TestCheck(t *testing.T) {
 	}
 
 	for i, test := range tests {
-		cert, key, err := genCertAndKey(test.startDate, time.Hour*24*time.Duration(test.daysRemaining))
+		cert, err := genTestCert(test.startDate, time.Hour*24*time.Duration(test.daysRemaining))
 		if err != nil {
-			t.Fatalf("error generating test cert and key: %v", err)
-		}
-
-		tlsCert, err := tls.X509KeyPair(cert, key)
-		if err != nil {
-			t.Fatalf("error parsing test certificate: %v", err)
+			t.Fatalf("error generating test cert: %v", err)
 		}
 
 		s := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
-		s.TLS = &tls.Config{Certificates: []tls.Certificate{tlsCert}}
+		s.TLS = &tls.Config{Certificates: []tls.Certificate{cert}}
 		s.StartTLS()
 
 		_, port, err := net.SplitHostPort(s.Listener.Addr().String())
@@ -69,17 +64,18 @@ func TestCheck(t *testing.T) {
 	}
 }
 
-// Generate a self-signed X.509 cert and private key for testing a TLS server
-func genCertAndKey(startDate time.Time, duration time.Duration) ([]byte, []byte, error) {
+// Generate a self-signed cert for testing a TLS server. Start date and duration
+// of the cert are configurable.
+func genTestCert(startDate time.Time, duration time.Duration) (tls.Certificate, error) {
 	priv, err := rsa.GenerateKey(rand.Reader, 1024)
 	if err != nil {
-		return nil, nil, err
+		return tls.Certificate{}, err
 	}
 
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
 	if err != nil {
-		return nil, nil, err
+		return tls.Certificate{}, err
 	}
 
 	template := x509.Certificate{
@@ -104,10 +100,16 @@ func genCertAndKey(startDate time.Time, duration time.Duration) ([]byte, []byte,
 
 	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
 	if err != nil {
-		return nil, nil, err
+		return tls.Certificate{}, err
 	}
 
 	cert := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
 	key := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
-	return cert, key, nil
+
+	tlsCert, err := tls.X509KeyPair(cert, key)
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+
+	return tlsCert, nil
 }
